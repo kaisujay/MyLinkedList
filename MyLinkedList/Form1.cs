ee3b9662d99c98f5f3165d91d4d69c4a28c420fb
sujay <sujay@DESKTOP-FHS1GO0>
2022-03-18 18:19:31 +0530

    Add project files.

---
+++ MyLinkedList/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyLinkedList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {            
+            MySingleLinkedList list = new MySingleLinkedList();
+            list.AddLast(3);
+            list.AddLast(5);
+            list.AddLast(7);
+            list.AddLast(9);
+            list.AddLast(11);
+            richTextBox1.Text = list.Display();
+            list.AddFirst(10);
+            list.AddFirst(1);
+            list.AddLast(2);
+            richTextBox1.Text = list.Display();
+            list.AddBefore(11,55);
+            richTextBox1.Text = list.Display();
+            list.AddAfter(5, 77);
+            richTextBox1.Text = list.Display();
+            // --> 1 --> 10 --> 3 --> 5 --> 7 --> 9 --> 11 --> 2
+            // --> 1 --> 10 --> 3 --> 5 --> 7 --> 9 --> 55 --> 11 --> 2
+            // --> 1 --> 10 --> 3 --> 5 --> 77 --> 7 --> 9 --> 55 --> 11 --> 2
+        }
+    }
+}
---
+++ MyLinkedList/MySingleLinkedList.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLinkedList
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+    }
+
+    public class MySingleLinkedList
+    {
+        public Node head;
+
+        public string Display()
+        {
+            string result = "";
+
+            Node current = new Node();
+            current = head;         // We will start from 1st Node and check all the Nodes
+
+            while (current != null)
+            {
+                result = result + " --> " + current.data;
+                current = current.next;
+            }
+            return result;
+        }
+
+        public void AddLast(int value)
+        {
+            if (head == null)
+            {
+                head = new Node();
+                head.data = value;          //Here we are saving the HEAD and it will be SAVED for later time also
+            }
+            else
+            {
+                Node newNode = new Node();
+                newNode.data = value;
+
+                Node current = new Node();
+                current = head;
+
+                while (current.next != null)
+                {
+                    current = current.next;
+                }
+                current.next = newNode;     // We need to pass a NODE not just DATA
+            }
+        }
+
+        public void AddFirst(int value)
+        {
+            Node newNode = new Node();
+            newNode.data = value;
+            newNode.next = head;
+
+            head = newNode;     //NEW NODE is the starting point now
+        }
+
+        public void AddBefore(int findValue,int value)
+        {
+            Node newNode = new Node();
+            newNode.data = value;
+
+            // TEMP NODE for saving the previous Node as we can't go backward (Like Doublely Linked List)
+            Node tempPreviousNode = new Node();
+
+            Node current = new Node();
+            current = head;
+
+            while (current != null)
+            {
+                if (findValue == current.data)
+                {
+                    newNode.next = current;
+                    tempPreviousNode.next = newNode;
+                }
+                else
+                {
+                    tempPreviousNode = current;
+                }
+                current = current.next;
+            }
+        }
+
+        public void AddAfter(int findValue, int value)
+        {
+            Node newNode = new Node();
+            newNode.data = value;
+
+            Node current = new Node();
+            current = head;
+
+            while (current != null)
+            {
+                if (current.data == findValue)
+                {
+                    newNode.next= current.next;
+                    current.next = newNode;
+                }
+                current = current.next;                
+            }
+        }
+    }
+}
